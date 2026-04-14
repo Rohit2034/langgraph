7f71d1426cf50c855bc3de01189d9af4a6242191
Rohit2034 <303370@ust.com>
2026-04-14 14:46:46 +0530

    threading with frontend

--- streamlit_chatbot/lanngraph_backend.py
+++ streamlit_chatbot/lanngraph_backend.py
@@ -1,3 +1,34 @@
+
+import ssl
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+ssl._create_default_https_context = ssl._create_unverified_context
+try:
+    import httpx
+    _original_client = httpx.Client
+    _original_async_client = httpx.AsyncClient
+    class PatchedClient(httpx.Client):
+        def __init__(self, *args, **kwargs):
+            kwargs['verify'] = False
+            super().__init__(*args, **kwargs)
+    class PatchedAsyncClient(httpx.AsyncClient):
+        def __init__(self, *args, **kwargs):
+            kwargs['verify'] = False
+            super().__init__(*args, **kwargs)
+    httpx.Client = PatchedClient
+    httpx.AsyncClient = PatchedAsyncClient
+except Exception:
+    pass
+try:
+    import requests
+    original_request = requests.Session.request
+    def patched_request(self, *args, **kwargs):
+        kwargs['verify'] = False
+        return original_request(self, *args, **kwargs)
+    requests.Session.request = patched_request
+except Exception:
+    pass
+
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage
